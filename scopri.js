6cc5f43fcf493ff9f092b0e9b0edc1c580bbeefe
edocrose <95345338+edocrose@users.noreply.github.com>
2023-08-10 11:04:00 +0200

    Delete scopri.js

--- scopri.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var learnMoreButton = document.getElementById("learnMoreButton");
-
-    learnMoreButton.addEventListener("click", function () {
-        window.location.href = "spider_man_story.html";
-    });
-});
