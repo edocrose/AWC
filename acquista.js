6b7960911d6d17a7fddea042f43b8c8737ac4d8d
Dimax1-1 <115618293+Dimax1-1@users.noreply.github.com>
2023-08-24 12:24:24 +0200

    Add files via upload

--- acquista.js
+++ acquista.js
@@ -1,29 +1,32 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const acquistaButton = document.getElementById("acquistaButton");
-    const pacchetti = document.querySelectorAll(".pacchetto");
+    const pacchetti = document.querySelectorAll(".descrizione");
 
     pacchetti.forEach(function (pacchetto) {
         const minusButton = pacchetto.querySelector(".minus");
         const plusButton = pacchetto.querySelector(".plus");
         const quantitySpan = pacchetto.querySelector(".quantity");
+        const priceSpan = pacchetto.querySelector(".price");
 
-        let quantity = 0;
+        let quantity = 1;
+        const basePrice = 5; // Cambia questo valore al prezzo base del pacchetto
 
         minusButton.addEventListener("click", function () {
-            if (quantity > 0) {
+            if (quantity > 1) {
                 quantity--;
                 quantitySpan.textContent = quantity;
+                updatePrice();
             }
         });
 
         plusButton.addEventListener("click", function () {
             quantity++;
             quantitySpan.textContent = quantity;
+            updatePrice();
         });
-    });
 
-    acquistaButton.addEventListener("click", function () {
-        // Implementa il tuo codice per l'acquisto dei pacchetti
-        // Puoi utilizzare la variabile 'quantity' per ottenere la quantità selezionata
+        function updatePrice() {
+            const totalPrice = basePrice * quantity;
+            priceSpan.textContent = totalPrice;
+        }
     });
 });
