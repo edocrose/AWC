6d56a2efb6d0188d7b4d9ebd817b097e63e4000d
Dimax1-1 <115618293+Dimax1-1@users.noreply.github.com>
2023-09-02 17:38:16 +0200

    Modifiche
    RICHIESTA URGENTE: UPDATE DI ACQUISTA.JS DA PARTE DI EDOCROSE !!!
    file modificati:
     css (da rivedere):
    - dettagli.css: guardare come far stare descrizione sotto all'immagine / mettere descrizione insieme al div obj
    - card.css: guadare grandezza carte
     html:
    - cartaDettagli.html: aggiunta linea rossa per il css 
    - home.html: sistemato FINALMENTE e pronto per visualizzare le carte dell'utente
    js:
    - mazzo.js: modifiche apportate per far sì che si creassero i character-card
    - storiaCarta.js: corretto un errore da events_ev a ul_ev (ATTENZIONE: modificare il messaggio per quando array è vuoto)

--- mazzo.js
+++ mazzo.js
@@ -12,19 +12,33 @@ for (var i = 0; i < json.length; i++) {
 }
 
 function cards(user) {
-    var card = document.getElementById('character-card');
+    var cardContainer = document.querySelector('.contenitore');
+
     for (var i = 0; i < user.carte.length; i++) {
-            
-        var clone = card.cloneNode(true);
-        clone.id = 'badge-' + i;
+        var characterCard = document.createElement('div');
+        characterCard.id = 'character-card';
+        characterCard.classList.add('badge');
 
-        clone.getElementsByClassName('card-title')[0].innerHTML = user.carte[i].name;
-          
-        clone.getElementsByClassName('card-img-top')[0].src = user.carte[i].thumbnail.path+"."+user.carte[i].thumbnail.extension;
-        card.after(clone);
+        var img = document.createElement('img');
+        img.classList.add('card-img-top');
+        img.alt = '...';
+        img.src = user.carte[i].thumbnail.path + "." + user.carte[i].thumbnail.extension;
 
-        clone.getElementsByClassName('btn-primary')[0].href = clone.getElementsByClassName('btn-primary')[0].href + user.carte[i].id + "&username=" + user.username;
+        var h2 = document.createElement('h2');
+        h2.classList.add('card-title');
+        h2.innerHTML = user.carte[i].name;
+
+        var a = document.createElement('a');
+        a.classList.add('btn-primary');
+        a.href = 'scheda-supereroe.html?id=' + user.carte[i].id + '&username=' + user.username;
+        a.innerHTML = 'Scopri di più';
+
+        characterCard.appendChild(img);
+        characterCard.appendChild(h2);
+        characterCard.appendChild(a);
+
+        cardContainer.appendChild(characterCard); // Aggiungi il div clonato come figlio del contenitore
     }
-
-
 }
+
+
