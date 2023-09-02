6ec2e398d6bfca2563c6fe42c21a5dfc6dce7865
edocrose <95345338+edocrose@users.noreply.github.com>
2023-09-02 12:30:47 +0200

    Update

--- pacchetto.js
+++ pacchetto.js
@@ -7,10 +7,6 @@ imageElement1.src = pack[0].thumbnail.path+"."+pack[0].thumbnail.extension;
 imageElement1.id = pack[0].id
 const nameElement1 = document.createElement("h2");
 nameElement1.textContent = pack[0].name;
-const descrizione1 = pack[0].description
-const comics1 = pack[0].comics.items.name
-const series1 = pack[0].series.items.name
-const events1 = pack[0].events.items.name
 
 // Aggiungi gli elementi alla pagina dei dettagli
 i1.appendChild(imageElement1);
@@ -22,10 +18,6 @@ const imageElement2 = document.createElement("img");
 imageElement2.src = pack[1].thumbnail.path+"."+pack[1].thumbnail.extension;
 const nameElement2 = document.createElement("h2");
 nameElement2.textContent = pack[1].name;
-const descrizione2 = pack[1].description
-const comics2 = pack[1].comics.items.name
-const series2 = pack[1].series.items.name
-const events2 = pack[1].events.items.name
 // Aggiungi gli elementi alla pagina dei dettagli
 i2.appendChild(imageElement2);
 i2.appendChild(nameElement2);
@@ -36,10 +28,6 @@ const imageElement3 = document.createElement("img");
 imageElement3.src = pack[2].thumbnail.path+"."+pack[2].thumbnail.extension;
 const nameElement3 = document.createElement("h2");
 nameElement3.textContent = pack[2].name;
-const descrizione3 = pack[2].description
-const comics3 = pack[2].comics.items.name
-const series3 = pack[2].series.items.name
-const events3 = pack[2].events.items.name
 // Aggiungi gli elementi alla pagina dei dettagli
 i3.appendChild(imageElement3);
 i3.appendChild(nameElement3);
@@ -50,10 +38,6 @@ const imageElement4 = document.createElement("img");
 imageElement4.src = pack[3].thumbnail.path+"."+pack[3].thumbnail.extension;
 const nameElement4 = document.createElement("h2");
 nameElement4.textContent = pack[3].name;
-const descrizione4 = pack[3].description
-const comics4 = pack[3].comics.items.name
-const series4 = pack[3].series.items.name
-const events4 = pack[3].events.items.name
 // Aggiungi gli elementi alla pagina dei dettagli
 i4.appendChild(imageElement4);
 i4.appendChild(nameElement4);
@@ -64,10 +48,6 @@ const imageElement5 = document.createElement("img");
 imageElement5.src = pack[4].thumbnail.path+"."+pack[4].thumbnail.extension;
 const nameElement5 = document.createElement("h2");
 nameElement5.textContent = pack[4].name;
-const descrizione5 = pack[4].description
-const comics5 = pack[4].comics.items.name
-const series5 = pack[4].series.items.name
-const events5 = pack[4].events.items.name
 // Aggiungi gli elementi alla pagina dei dettagli
 i5.appendChild(imageElement5);
 i5.appendChild(nameElement5);
@@ -87,66 +67,31 @@ function redirectNegozio(){
 }
 
 function mostraDettagliCarta1(){
-    const carta1 = {
-        immagine: imageElement1.src, 
-        nome: nameElement1,
-        descrizione: descrizione1,
-        comics: comics1,
-        series: series1,
-        events: events1
-    }
-    localStorage.setItem("cartaDettagli", JSON.stringify(carta1))
-    window.location.href = "cartaDettagli.html"
+    const urlParams = new URLSearchParams(window.location.search);
+    const username = urlParams.get("username");
+    window.location.href = "cartaDettagli.html?username="+username+"&carta="+0;
 }
 
 function mostraDettagliCarta2(){
-    const carta2 = {
-        immagine: imageElement2.src, 
-        nome: nameElement2,
-        descrizione: descrizione2,
-        comics: comics2,
-        series: series2,
-        events: events2
-    }
-    localStorage.setItem("cartaDettagli", JSON.stringify(carta2))
-    window.location.href = "cartaDettagli.html"
+    const urlParams = new URLSearchParams(window.location.search);
+    const username = urlParams.get("username");
+    window.location.href = "cartaDettagli.html?username="+username+"&carta="+1;
 }
 
 function mostraDettagliCarta3(){
-    const carta3 = {
-        immagine: imageElement3.src, 
-        nome: nameElement3,
-        descrizione: descrizione3,
-        comics: comics3,
-        series: series3,
-        events: events3
-    }
-    localStorage.setItem("cartaDettagli", JSON.stringify(carta3))
-    window.location.href = "cartaDettagli.html"
+    const urlParams = new URLSearchParams(window.location.search);
+    const username = urlParams.get("username");
+    window.location.href = "cartaDettagli.html?username="+username+"&carta="+2;
 }
 
 function mostraDettagliCarta4(){
-    const carta4 = {
-        immagine: imageElement4.src, 
-        nome: nameElement4,
-        descrizione: descrizione4,
-        comics: comics4,
-        series: series4,
-        events: events4
-    }
-    localStorage.setItem("cartaDettagli", JSON.stringify(carta4))
-    window.location.href = "cartaDettagli.html"
+    const urlParams = new URLSearchParams(window.location.search);
+    const username = urlParams.get("username");
+    window.location.href = "cartaDettagli.html?username="+username+"&carta="+3;
 }
 
 function mostraDettagliCarta5(){
-    const carta5 = {
-        immagine: imageElement5.src, 
-        nome: nameElement5,
-        descrizione: descrizione5,
-        comics: comics5,
-        series: series5,
-        events: events5
-    }
-    localStorage.setItem("cartaDettagli", JSON.stringify(carta5))
-    window.location.href = "cartaDettagli.html"
+    const urlParams = new URLSearchParams(window.location.search);
+    const username = urlParams.get("username");
+    window.location.href = "cartaDettagli.html?username="+username+"&carta="+4;
 }
--- storiaCarta.js
+++ storiaCarta.js
@@ -1,24 +1,66 @@
 //nome,immagine, descrizione, comics, serie, eventi
-var json = localStorage.getItem('carta1')
-var carta = JSON.parse(json);
+var json = localStorage.getItem('pacchetto')
+var pack = JSON.parse(json);
 
-const contenuto = document.querySelector(".contenuto")
+const urlParams = new URLSearchParams(window.location.search);
+const index = urlParams.get("carta");
+const carta = pack[index];
+
+const contenuto = document.querySelector(".text")
 const immagine = document.createElement("img")
-immagine.src = carta.immagine
+immagine.src = carta.thumbnail.path+"."+carta.thumbnail.extension
 const nome = document.createElement("h2")
-nome = carta.nome
+nome.textContent = carta.name
 const descrizione = document.createElement("p")
-descrizione = carta.descrizione
-const comics = document.createElement("p")
-comics = carta.comics
-const series = document.createElement("p")
-series = carta.series
-const events = document.createElement("p")
-events = carta.events
+descrizione.textContent = carta.description
+
+const comics = document.querySelector(".comics")
+const ul_com = document.createElement('ul');
+if(carta.comics.items.length == 0){
+    const p = document.createElement("p");
+    p.textContent="Comics Not Found XDXDXDXD"
+    comics.appendChild(p)
+} else {
+    for(let i=0; i<carta.comics.items.length; i++){
+        const li = document.createElement('li');
+        li.textContent = carta.comics.items[i].name;
+        ul_com.appendChild(li);
+    }
+}
+
+
+const series = document.querySelector(".series")
+const ul_ser = document.createElement('ul');
+if(carta.series.items.length == 0){
+    const p = document.createElement("p");
+    p.textContent="Series Not Found XDXDXDXD"
+    series.appendChild(p)
+} else {
+    for(let i=0; i<carta.series.items.length; i++){
+        const li = document.createElement('li');
+        li.textContent = carta.series.items[i].name;
+        ul_ser.appendChild(li);
+    }
+}
+
+const events = document.querySelector(".events")
+const ul_ev = document.createElement('ul');
+if(carta.events.items.length == 0){
+    const p = document.createElement("p");
+    p.textContent="Events Not Found XDXDXDXD"
+    events.appendChild(p)
+}
+for(let i=0; i<carta.events.items.length; i++){
+    const li = document.createElement('li');
+    li.textContent = carta.events.items[i].name;
+    ul_ev.appendChild(li);
+}
+
+
 
 contenuto.appendChild(immagine)
 contenuto.appendChild(nome)
 contenuto.appendChild(descrizione)
-contenuto.appendChild(comics)
-contenuto.appendChild(series)
-contenuto.appendChild(events)
+comics.appendChild(ul_com)
+series.appendChild(ul_ser)
+events.appendChild(events_ev)
