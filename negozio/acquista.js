ad07c5a7da7f1e2438adccc9497122f4f071f232
Dimax1-1 <115618293+Dimax1-1@users.noreply.github.com>
2023-09-07 14:16:31 +0200

    Add files via upload

--- negozio/acquista.js
+++ negozio/acquista.js
@@ -30,7 +30,7 @@ function trovaUtente() {
 
 function controllaDoppie(mazzo, card){
     for(let i=0; i<mazzo.length; i++){
-        if(card.id == mazzo[i].id){
+        if(card == mazzo[i]){
             return true;
         }
     }
@@ -42,38 +42,13 @@ document.addEventListener("DOMContentLoaded", function () {
     var utente = trovaUtente();
 
     pacchetti.forEach(function (pacchetto) {
-        const minusButton = pacchetto.querySelector(".minus");
-        const plusButton = pacchetto.querySelector(".plus");
-        const quantitySpan = pacchetto.querySelector(".quantity");
         const priceSpan = pacchetto.querySelector(".price");
         const acquistaButton = pacchetto.querySelector(".acquista");
 
-        let quantity = 1;
-        const basePrice = 1; // Cambia questo valore al prezzo base del pacchetto
-
-        minusButton.addEventListener("click", function () {
-            if (quantity > 1) {
-                quantity--;
-                quantitySpan.textContent = quantity;
-                updatePrice();
-            }
-        });
-
-        plusButton.addEventListener("click", function () {
-            quantity++;
-            quantitySpan.textContent = quantity;
-            updatePrice();
-        });
-
-        function updatePrice() {
-            const totalPrice = basePrice * quantity;
-            priceSpan.textContent = totalPrice;
-            return priceSpan.textContent
-        }
 
         //funzione acquista
         acquistaButton.addEventListener("click", async () =>{
-            const packPrice = updatePrice();
+            const packPrice = priceSpan.textContent;
             if (utente.credits >= packPrice) {
                 const urlParams = new URLSearchParams(window.location.search);
                 const username = urlParams.get("username");
