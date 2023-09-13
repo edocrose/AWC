c47eb23b086f569d8c621e1dd713e1ee6b291837
Dimax1-1 <115618293+Dimax1-1@users.noreply.github.com>
2023-09-13 12:21:01 +0200

    Delete form_script.js

--- form_script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var userIcon = document.getElementById("userIcon");
-    var userForm = document.getElementById("userForm");
-
-    userIcon.addEventListener("click", function () {
-        userForm.classList.toggle("hidden");
-    });
-});
